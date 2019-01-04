15bda3ccf0de59251ba182ed1a5b4531142cd6ec
Brian Shimkus <brianshimkus1@gmail.com>
2019-01-04 11:57:57 -0600

    Built out minute hand functionality

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
 
 function setDate() {
   const now = new Date();
+  
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360;
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes);
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  
   console.log(seconds);
 }
 
