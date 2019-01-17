aea9263839d08c024dad53a65a50310fa94fd578
Brian Shimkus <brianshimkus1@gmail.com>
2019-01-17 03:10:35 -0600

    Set mouse location to only register on mousedown

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ let lastX = 0;
 let lastY = 0;
 
 function draw(e) {
+  if(!isDrawing) return;
   console.log(e);
 }
 
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
