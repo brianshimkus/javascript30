9466f914a6fc6e20e183b2a71330ef6b5b26c189
Brian Shimkus <brianshimkus1@gmail.com>
2019-01-18 23:59:00 -0600

    Console exercises

--- script.js
+++ script.js
@@ -7,25 +7,48 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
     }
 
     // Regular
+    console.log('hello');
 
     // Interpolated
+    console.log('Hello, I am a %s string!', '10');
 
     // Styled
+    console.log('%c I am some great text', 'font-size: 50px;');
 
     // warning!
+    console.warn("Oh no");
 
     // Error :|
+    console.error("Oh no");
 
     // Info
+    console.info('People eat food');
 
     // Testing
+    console.assert(1 === 2);
 
     // clearing
+    // console.clear("hello");
 
     // Viewing DOM Elements
+    console.dir('p');
 
     // Grouping together
+    cats.forEach(cat => {
+      console.log(`This is ${cat.name}`);
+      console.log(`${cat.name} is ${cat.age} years old`);
+      console.log(`${cat.name} is ${cat.age * 6} cat years old`);
+      console.groupEnd(`${cat.name}`);
+    });
 
     // counting
+    console.count('Oh');
+    console.count('Oh');
+    console.count('hi');
+    console.count('hi');
+    console.count('hi');
+    console.count('bye');
+    console.count('bye');
 
     // timing
+    console.time('fetch data');
