3574b3d163c9c40a93d0c843023957d6204aa1c3
Brian Shimkus <brianshimkus1@gmail.com>
2019-01-29 23:39:00 -0600

    Built out functionality for range sliders

--- script.js
+++ script.js
@@ -22,8 +22,11 @@ function updateButton() {
 }
 
 function skip() {
-  console.log(this.dataset.skip);
   video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
 }
 
 // Hook up event listeners
@@ -33,3 +36,5 @@ video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
