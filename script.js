dbdcd71ac0ca649c7f6b248f8ae6d9e579035120
Brian Shimkus <brianshimkus1@gmail.com>
2019-02-01 23:35:01 -0600

    Check to see if secret code has been typed

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const pressed = [];
+const secretCode = 'brian'; 
+
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length -1, pressed.length - secretCode.length);
+  if(pressed.join('').includes(secretCode)) {
+    console.log('There it is.');
+  }
+  console.log(pressed);
 })
