75bc8e1c0b68cafa21831482b0bf0d7b6762aded
Brian Shimkus <brianshimkus1@gmail.com>
2019-07-13 23:55:50 -0500

    Got video to display on the page

--- 19 - Webcam Fun/script.js
+++ 19 - Webcam Fun/script.js
@@ -9,7 +9,23 @@ function getVideo() {
     .getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`Error`, err);
     });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.height = height;
+  canvas.width = width;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
 }
 
 getVideo();
