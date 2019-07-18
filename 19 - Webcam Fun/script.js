68a39f63f36494d32f1d30a535759a9bbb8a0d36
Brian Shimkus <brianshimkus1@gmail.com>
2019-07-17 23:12:20 -0500

    Built out functionality to actually take the photo

--- 19 - Webcam Fun/script.js
+++ 19 - Webcam Fun/script.js
@@ -9,7 +9,7 @@ function getVideo() {
     .getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
-      video.src = window.URL.createObjectURL(localMediaStream);
+      video.srcObject = localMediaStream;
       video.play();
     })
     .catch(err => {
@@ -28,4 +28,20 @@ function paintToCanvas() {
   }, 16);
 }
 
+function takePhoto() {
+  // plays the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement(`a`);
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
