1ca72f22ebc614cf075ae33af0e706e1c7294eaf
Brian Shimkus <brianshimkus1@gmail.com>
2019-07-23 00:17:23 -0500

    Fixed bug and added error message if user doesn't allow access

--- 21 - Geolocation/script.js
+++ 21 - Geolocation/script.js
@@ -1,7 +1,15 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
-NavigationPreloadManager.geolocation.watchPosition(data => {
-  speed.textContent = data.coords.speed;
-  arrow.getElementsByClassName.transform = `rotate(${data.coords.heading}deg)`;
-});
+navigator.geolocation.watchPosition(
+  data => {
+    speed.textContent = data.coords.speed;
+    arrow.getElementsByClassName.transform = `rotate(${
+      data.coords.heading
+    }deg)`;
+  },
+  err => {
+    console.log(err);
+    alert('Allow access in order to run');
+  }
+);
