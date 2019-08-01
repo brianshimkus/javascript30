a427a6ac9c7fec80a4466b16a88bdb9c43c6b5ea
Brian Shimkus <brianshimkus1@gmail.com>
2019-08-01 00:00:51 -0500

    Calculated percent of mouseover movement

---
+++ 28 - Video Speed Controller UI/script.js
@@ -0,0 +1,9 @@
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+speed.addEventListener('mousemove', function(e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  console.log(percent);
+});
