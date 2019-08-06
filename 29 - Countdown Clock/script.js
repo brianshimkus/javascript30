80c690fb3647e77091a688d60270fe3354f1f573
Brian Shimkus <brianshimkus1@gmail.com>
2019-08-05 22:57:46 -0500

    Timer stops at 0

--- 29 - Countdown Clock/script.js
+++ 29 - Countdown Clock/script.js
@@ -1,9 +1,16 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
 
-  setInterval(() => {
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if clock should stop
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
     console.log(secondsLeft);
   }, 1000);
 }
